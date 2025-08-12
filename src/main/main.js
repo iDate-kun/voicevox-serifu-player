d68af9f818a179f2fe8b38aadf23f538f8ed5795
iDate-kun <minaminosimaniiruminami373@gmail.com>
2025-08-12 17:05:13 +0900

    アイコンの追加

--- src/main/main.js
+++ src/main/main.js
@@ -261,6 +261,7 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join('S:', 'gemini-code', 'voicevox-serihu', 'assets', 'voicevox-serihu-player-icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
@@ -274,11 +275,11 @@ function createWindow() {
 
 app.whenReady().then(() => {
   createWindow();
-  app.on('activate', function () {
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
